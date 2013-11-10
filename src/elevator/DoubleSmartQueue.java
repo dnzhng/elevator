b80f47d6d6917f8c1df14b01b89de0aece50e9bc
thedanzhang <dyz2@duke.edu>
2013-11-10 17:52:28 -0500

    Fixed DoubleSmartQueue, hope it makes sense

--- src/elevator/DoubleSmartQueue.java
+++ src/elevator/DoubleSmartQueue.java
@@ -12,55 +12,22 @@ import building.elevator_controller.Direction;
 
 public class DoubleSmartQueue<E extends Comparable<E>> implements Queue<E> {
 
-	private Node<E> myHead;
-	private Node<E> myTail;
 	
-	private SmartQueue<Integer> myUpQueue;
-	private SmartQueue<Integer> myDownQueue;
+	private SmartQueue<E> myUpQueue;
+	private SmartQueue<E> myDownQueue;
 
-	private int mySize = 0;
-		
-	private class Node<E>{
-		E myValue;
-		Node<E> myNext;
-	}
-	
-	
 
 	/**
 	 * Is the queue in ascending or descending order?
 	 * @return
 	 */
-	public Direction getDirection(){
-		
-		if(mySize < 2){
-			return null;
-		}
-		else{
-			E val1 = myHead.myValue;
-			E val2 = myHead.myNext.myValue;
-			
-			if(val2.compareTo(val1) > 0){
-				return Direction.UP;
-			}
-			else{
-				return Direction.DOWN;
-			}
-		}
-	}
 	
-	public SmartQueue<Integer> getUpQueue() {
+	public SmartQueue<E> getUpQueue() {
 		return myUpQueue;
 	}
 	
-	public SmartQueue<Integer> getDownQueue() {
+	public SmartQueue<E> getDownQueue() {
 		return myDownQueue;
-	}
-	
-	@Override
-	public boolean addAll(Collection<? extends E> arg0) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 	@Override
@@ -77,46 +44,15 @@ public class DoubleSmartQueue<E extends Comparable<E>> implements Queue<E> {
 	}
 
 	@Override
-	public boolean containsAll(Collection<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
 	public boolean isEmpty() {
 		return myUpQueue.isEmpty() || myDownQueue.isEmpty();
 	}
 
 	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public boolean remove(Object arg0) {
 		
-		myUpQueue.remove(arg0);
-		myDownQueue.remove(arg0);
-	}
-	
-//	private void removeNode(Node<E> prev, Node<E> toBeRemoved){
-//		
-//		if (myUpQueue.contains(toBeRemoved)) 
-//			myUpQueue.removeNode()
-//	}
-	
+		return myUpQueue.remove(arg0) || myDownQueue.remove(arg0);
 
-	@Override
-	public boolean removeAll(Collection<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean retainAll(Collection<?> arg0) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 	public int getUpSize() {
@@ -128,28 +64,81 @@ public class DoubleSmartQueue<E extends Comparable<E>> implements Queue<E> {
 	}
 
 	@Override
+	public boolean add(E arg0) {
+		if (arg0.compareTo(myUpQueue.peek()) >= 0 ) {
+			return myUpQueue.add(arg0);
+		}
+		else {
+			return myDownQueue.add(arg0);
+		}
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return myUpQueue.size() + myDownQueue.size();
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
 	public Object[] toArray() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public <T> T[] toArray(T[] arg0) {
+	public <T> T[] toArray(T[] a) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public boolean add(E arg0) {
-		if (arg0.compareTo((E) myUpQueue.peek())) {
-			myUpQueue.add(arg0);
-		}
-		else {
-			myDownQueue.add(arg0);
-		}
+	public boolean containsAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
-	
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean offer(E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	@Override
 	public E element() {
 		// TODO Auto-generated method stub
@@ -157,55 +146,9 @@ public class DoubleSmartQueue<E extends Comparable<E>> implements Queue<E> {
 	}
 
 	@Override
-	public boolean offer(E arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
 	public E peek() {
-		
-		if(myHead != null){
-			return myHead.myValue;
-		}
-		
+		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	public E poll() {
-		if(myHead != null){
-			E val = myHead.myValue;	
-			myHead = myHead.myNext;
-			return val;
-		}
-		
-		return null;
-	}
-
-	@Override
-	public E remove() {
-		if(myHead == null){
-			throw new NoSuchElementException();
-		}
-		else{
-			E val = myHead.myValue;
-			myHead = myHead.myNext;
-			return val;
-		}
-	}
-
-	@Override
-	public String toString(){
-		Node<E> current = myHead;
-		StringBuffer sb = new StringBuffer();
-		
-		while(current != null){
-			sb.append(",");
-			sb.append(current.myValue.toString());
-			current = current.myNext;
-		}
-		return sb.toString();
 	}
 	
 }
